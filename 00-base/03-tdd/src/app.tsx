c5b78d1985018df017c9f2f56f1b10e779d6ba1c
nasdan <idsm00012@gmail.com>
2019-06-20 22:10:06 +0200

    Implement tdd example

--- 00-base/03-tdd/src/app.tsx
+++ 00-base/03-tdd/src/app.tsx
@@ -1,5 +1,12 @@
 import * as React from 'react';
+import { getMembers } from './api';
 
 export const App: React.FunctionComponent = () => {
+  React.useEffect(() => {
+    getMembers().then(members => {
+      console.log(members);
+    });
+  });
+
   return <h1>React testing by sample</h1>;
 };
