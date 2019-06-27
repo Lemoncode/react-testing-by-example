856fcb370924783d1612e6d163a277da26d79527
nasdan <idsm00012@gmail.com>
2019-06-27 20:58:01 +0200

    implement 02-use-state-object example

---
+++ 02-hooks/02-use-state-object/src/useUser.spec.ts
@@ -0,0 +1,43 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import { User } from './model';
+import { useUser } from './useUser';
+
+describe('useUser specs', () => {
+  it('should return user with initial values and setUser method when it calls it', () => {
+    // Arrange
+    const initialUser: User = {
+      name: 'John',
+      surname: 'Doe',
+    };
+
+    // Act
+    const { result } = renderHook(() => useUser(initialUser));
+
+    // Asser
+    expect(result.current.user).toEqual(initialUser);
+    expect(result.current.setUser).toEqual(expect.any(Function));
+  });
+
+  it('should update user when it calls setUser', () => {
+    // Arrange
+    const initialUser: User = {
+      name: 'John',
+      surname: 'Doe',
+    };
+
+    // Act
+    const { result } = renderHook(() => useUser(initialUser));
+
+    act(() => {
+      result.current.setUser({
+        name: 'updated name',
+        surname: 'updated surname',
+      });
+    });
+    // Asser
+    expect(result.current.user).toEqual({
+      name: 'updated name',
+      surname: 'updated surname',
+    });
+  });
+});
