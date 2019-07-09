a5bd44b78fc22831d0706b76dbbee744c0e199fb
Santiago Camargo Rodríguez <santi.camargo.rodriguez@gmail.com>
2019-07-09 20:48:26 +0200

    Fixed pages/sagas test

--- 03-redux/03-sagas/src/pages/sagas.ts
+++ 03-redux/03-sagas/src/pages/sagas.ts
@@ -1,6 +1,5 @@
-import { all } from 'redux-saga/effects';
 import { membersRootSaga } from './members';
 
 export function* rootSaga() {
-  yield all([membersRootSaga()]);
+  yield membersRootSaga();
 }
