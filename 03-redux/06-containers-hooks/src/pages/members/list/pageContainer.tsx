faca7f855b72a2347a43ec1f114cd550d2d06c9b
Santiago Camargo Rodríguez <santi.camargo.rodriguez@gmail.com>
2020-02-08 02:14:40 +0100

    sync files from 05

--- 03-redux/06-containers-hooks/src/pages/members/list/pageContainer.tsx
+++ 03-redux/06-containers-hooks/src/pages/members/list/pageContainer.tsx
@@ -1,38 +1,27 @@
 import * as React from 'react';
-import { connect } from 'react-redux';
-import { Dispatch } from 'redux';
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 import { State } from '../../reducers';
-import { MembersAction } from './actions';
 import { fetchMembersRequest } from './actions/fetchMembers';
 import { MemberListPage } from './page';
 import { getMembersListVM, getServerError } from './selectors';
-import { Member } from './viewModel';
 
 const mapStateToProps = (state: State) => ({
   members: getMembersListVM(state),
   serverError: getServerError(state),
 });
 
-const mapDispatchToProps = (dispatch: Dispatch<MembersAction>) => ({
-  fetchMembers: () => {
+const useFetchMembers = () => {
+  const dispatch = useDispatch();
+  return React.useCallback(() => {
     dispatch(fetchMembersRequest());
-  },
-});
+  }, [dispatch]);
+};
 
-interface Props {
-  members: Member[];
-  serverError: string | null;
-  fetchMembers: () => void;
-}
-
-const PageContainer: React.FunctionComponent<Props> = React.memo(props => {
-  const { members, serverError, fetchMembers } = props;
+export const MemberListPageContainer: React.FunctionComponent = () => {
+  const { members, serverError } = useSelector(mapStateToProps, shallowEqual);
+  const fetchMembers = useFetchMembers();
   React.useEffect(fetchMembers, []);
 
   return <MemberListPage members={members} serverError={serverError} />;
-});
+};
 
-export const MemberListPageContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(PageContainer);
